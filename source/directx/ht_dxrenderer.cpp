b70eb09fc53d94c9eade94cb712ed28dacf5b6a3
MattGuerrette <direct3Dtutorials@gmail.com>
2015-12-27 19:55:08 -0500

    added init directx renderer class

---
+++ source/directx/ht_dxrenderer.cpp
@@ -0,0 +1,58 @@
+/**
+**    Hatchit Engine
+**    Copyright(c) 2015 Third-Degree
+**
+**    GNU Lesser General Public License
+**    This file may be used under the terms of the GNU Lesser
+**    General Public License version 3 as published by the Free
+**    Software Foundation and appearing in the file LICENSE.LGPLv3 included
+**    in the packaging of this file. Please review the following information
+**    to ensure the GNU Lesser General Public License requirements
+**    will be met: https://www.gnu.org/licenses/lgpl.html
+**
+**/
+
+#include <ht_dxrenderer.h>
+
+namespace Hatchit {
+
+    namespace Graphics {
+
+        DXRenderer::DXRenderer()
+        {
+
+        }
+
+        DXRenderer::~DXRenderer()
+        {
+
+        }
+
+        bool DXRenderer::VInitialize()
+        {
+            return true;
+        }
+
+        void DXRenderer::VDeInitialize()
+        {
+
+        }
+
+        void DXRenderer::VSetClearColor(const Color& color)
+        {
+
+        }
+
+        void DXRenderer::VClearBuffer(ClearArgs args)
+        {
+
+        }
+
+        void DXRenderer::VPresent()
+        {
+        
+        }
+
+    }
+
+}
