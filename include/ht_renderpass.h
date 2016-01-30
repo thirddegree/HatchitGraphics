f0b9e3b1bfb175e159f6989e16435ef1569d74cf
Honeybunch <amt3824@g.rit.edu>
2016-01-29 20:59:18 -0500

    Added some basic virtual headers
    Added material interface header
    
    Added renderpass interface header that has some basic member variables
    
    Added rendertarget interface header that has some basic stubbed methods

---
+++ include/ht_renderpass.h
@@ -0,0 +1,49 @@
+/**
+**    Hatchit Engine
+**    Copyright(c) 2015 Third-Degree
+**
+**    GNU Lesser General Public License
+**    This file may be used under the terms of the GNU Lesser
+**    General Public License version 3 as published by the Free
+**    Software Foundation and appearing in the file LICENSE.LGPLv3 included
+**    in the packaging of this file. Please review the following information
+**    to ensure the GNU Lesser General Public License requirements
+**    will be met: https://www.gnu.org/licenses/lgpl.html
+**
+**/
+
+#pragma once
+
+#include <ht_platform.h>
+#include <ht_material.h>
+#include <ht_rendertarget.h>
+#include <ht_math.h>
+
+namespace Hatchit {
+
+    namespace Graphics {
+
+        class HT_API IRenderPass
+        {
+        public:
+            virtual ~IRenderPass();
+
+            //Will this be sent the Objects that it needs to render?
+            virtual VRender();
+
+        private:
+            //We're going need to send this to the shader
+            Vector3 eyePoint;
+
+            //Data representing *where* the objects will be viewed from in this pass
+            Matrix4 viewMatrix;
+            Matrix4 projectionMatrix;
+
+            //Which material we will be rendering with
+            IMaterial* material;
+
+            //Output
+            IRenderTarget* renderTarget;
+        };
+    }
+}
