75494360a5e895adb5cb20f171ca698f7d4d6f3f
MattGuerrette <direct3Dtutorials@gmail.com>
2016-04-12 21:25:11 -0400

    updated d3d12constantbuffer class.

--- include/directx/ht_directx.h
+++ include/directx/ht_directx.h
@@ -72,6 +72,22 @@ namespace Hatchit {
                     throw;
             }
 
+            inline UINT ConstantBufferByteSize(UINT byteSize)
+            {
+                // Constant buffers must be a multiple of the minimum hardware
+                // allocation size (usually 256 bytes).  So round up to nearest
+                // multiple of 256.  We do this by adding 255 and then masking off
+                // the lower 2 bytes which store all bits < 256.
+                // Example: Suppose byteSize = 300.
+                // (300 + 255) & ~255
+                // 555 & ~255
+                // 0x022B & ~0x00ff
+                // 0x022B & 0xff00
+                // 0x0200
+                // 512
+                return (byteSize + 255) & ~255;
+            }
+
             struct Vertex
             {
                 Math::Float3 position;
