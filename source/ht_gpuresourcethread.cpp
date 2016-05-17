1c26899e49373fb4918633ecdecb0d0c65d0a7d4
MattGuerrette <direct3Dtutorials@gmail.com>
2016-05-17 09:31:18 -0400

    updated gpuresourcethread with comments.

--- source/ht_gpuresourcethread.cpp
+++ source/ht_gpuresourcethread.cpp
@@ -42,6 +42,16 @@ namespace Hatchit
             return m_locked;
         }
 
+
+        /**
+        *   \fn GPUResourceThread::Load()
+        *   \brief Function processes a non-async GPUResourceRequest
+        *   \param request Pointer to GPUResourceRequest object
+        *
+        *   This function will process a GPUResourceRequest non-asynchronously
+        *   by pushing the request onto the request stack and then blocking the main thread
+        *   for processing.
+        */
         void GPUResourceThread::Load(GPUResourceRequest* request)
         {
             if (!m_alive)
@@ -55,6 +65,15 @@ namespace Hatchit
             m_locked = false;
         }
 
+        /**
+        *   \fn GPUResourceThread::LoadAsync()
+        *   \brief Function processes an async GPUResourceRequest
+        *   \param request Pointer to GPUResourceRequet object
+        *
+        *   This function will process a GPUResourceRequest asynchronously
+        *   by pushing the request onto the request stack. It does NOT
+        *   block the main thread, but loads asynchronously instead.
+        */
         void GPUResourceThread::LoadAsync(GPUResourceRequest* request)
         {
             if (!m_alive)
@@ -63,6 +82,12 @@ namespace Hatchit
             m_requests.push(request);
         }
 
+        /**
+        *   \fn GPUResourceThread::Kill()
+        *   \brief Function kills the thread
+        *
+        *   This function will kill the thread and join until finished.
+        */
         void GPUResourceThread::Kill()
         {
             m_alive = false;
@@ -70,13 +95,29 @@ namespace Hatchit
                 m_thread.join();
         }
 
+        /**
+        *   \fn GPUResourceThread::CreateTexture()
+        *   \brief Function creates a texture
+        *   \param file Path of texture file to load.
+        *   \param data Pointer to the base texture implementation to fill.
+        *   
+        *   This function will create a texture object on the GPU resource thread.
+        */
         void GPUResourceThread::CreateTexture(std::string file, void ** data)
         {
             Resource::TextureHandle handle = Resource::Texture::GetHandle(file, file);
 
             VCreateTextureBase(handle, data);
         }
-
+        
+        /**
+        *   \fn GPUResourceThread::CreateMaterial()
+        *   \brief Function creates a material
+        *   \param file Path of material file to load.
+        *   \param data Pointer to the base material implementation to load.
+        *
+        *   This function will create a materia object on the GPU resource thread.
+        */
         void GPUResourceThread::CreateMaterial(std::string file, void ** data)
         {
             Resource::MaterialHandle handle = Resource::Material::GetHandle(file, file);
@@ -84,6 +125,14 @@ namespace Hatchit
             VCreateMaterialBase(handle, data);
         }
 
+        /**
+        *   \fn GPUResourceThread::CreateRootLayout()
+        *   \brief Function creates a rootlayout
+        *   \param file Path to rootlayout file to load.
+        *   \param data Pointer to the base rootlayout implementation to fill.
+        *
+        *   This function will create a rootlayout object on the GPU resource thread.
+        */
         void GPUResourceThread::CreateRootLayout(std::string file, void ** data)
         {
             Resource::RootLayoutHandle handle = Resource::RootLayout::GetHandle(file, file);
@@ -91,6 +140,14 @@ namespace Hatchit
             VCreateRootLayoutBase(handle, data);
         }
 
+        /**
+        *   \fn GPUResourceThread::CreatePipeline()
+        *   \brief Function creates a pipeline
+        *   \param file Path to pipeline file to load.
+        *   \param data Pointer to base pipeline implementation to fill.
+        *
+        *   This function will create a pipeline object on the GPU resource thread.
+        */
         void GPUResourceThread::CreatePipeline(std::string file, void ** data)
         {
             Resource::PipelineHandle handle = Resource::Pipeline::GetHandle(file, file);
@@ -98,6 +155,14 @@ namespace Hatchit
             VCreatePipelineBase(handle, data);
         }
 
+        /**
+        *   \fn GPUResourceThread::CreateShader()
+        *   \brief Function creates a shader
+        *   \param file Path to shader file to load.
+        *   \param data Pointer to the base shader implementation to fill.
+        *
+        *   This function will create a shader object on the GPU resource thread.
+        */
         void GPUResourceThread::CreateShader(std::string file, void ** data)
         {
             Resource::ShaderHandle handle = Resource::Shader::GetHandle(file, file);
@@ -105,6 +170,14 @@ namespace Hatchit
             VCreateShaderBase(handle, data);
         }
 
+        /**
+        *   \fn GPUResourceThread::CreateRenderPass()
+        *   \brief Function creates a renderpass
+        *   \param file Path to renderpass file to load.
+        *   \param data Pointer to the base renderpass implementation to fill.
+        *
+        *   This function will create a renderpass object on the GPU resource thread.
+        */
         void GPUResourceThread::CreateRenderPass(std::string file, void ** data)
         {
             Resource::RenderPassHandle handle = Resource::RenderPass::GetHandle(file, file);
@@ -112,6 +185,14 @@ namespace Hatchit
             VCreateRenderPassBase(handle, data);
         }
 
+        /**
+        *   \fn GPUResourceThread::CreateRenderTarget()
+        *   \brief Function creates a rendertarget
+        *   \param file Path to rendertarget file to load.
+        *   \param data Pointer to the base rendertarget implementation to fill.
+        *
+        *   This function will create a rendertarget object on the GPU resource thread.
+        */
         void GPUResourceThread::CreateRenderTarget(std::string file, void ** data)
         {
             Resource::RenderTargetHandle handle = Resource::RenderTarget::GetHandle(file, file);
@@ -119,6 +200,14 @@ namespace Hatchit
             VCreateRenderTargetBase(handle, data);
         }
 
+        /**
+        *   \fn GPUResourceThread::CreateMesh()
+        *   \brief Function creates a mesh
+        *   \param file Path to the mesh file to load.
+        *   \param data Pointer to the base mesh implementation to fill.
+        *
+        *   This function will create a mesh object on the GPU resource thread.
+        */
         void GPUResourceThread::CreateMesh(std::string file, void ** data)
         {
             Resource::ModelHandle handle = Resource::Model::GetHandle(file, file);
@@ -126,6 +215,15 @@ namespace Hatchit
             VCreateMeshBase(handle, data);
         }
 
+
+        /**
+        *   \fn GPUResourceThread::ProcessTextureRequest()
+        *   \brief Function processes a texture request
+        *   \param request Pointer to TextureRequest.
+        *
+        *   This function will process a TextureRequest and create the texture
+        *   either non-async or async depending on the state of the GPU resource thread.
+        */
         void GPUResourceThread::ProcessTextureRequest(TextureRequest * request)
         {
             Resource::TextureHandle handle = Resource::Texture::GetHandle(request->file, request->file);
@@ -142,6 +240,14 @@ namespace Hatchit
             }
         }
 
+        /**
+        *   \fn GPUResourceThread::ProcessMaterialRequest()
+        *   \brief Function processes a material request
+        *   \param request Pointer to MaterialRequest.
+        *
+        *   This function will process a MaterialRequest and create the material
+        *   either non-async or async depending on the state of the GPU resource thread.
+        */
         void GPUResourceThread::ProcessMaterialRequest(MaterialRequest * request)
         {
             Resource::MaterialHandle handle = Resource::Material::GetHandle(request->file, request->file);
@@ -158,6 +264,14 @@ namespace Hatchit
             }
         }
 
+        /**
+        *   \fn GPUResourceThread::ProcessRootLayoutRequest()
+        *   \brief Function processes a rootlayout request
+        *   \param request Pointer to RootLayoutRequest.
+        *
+        *   This function will process a RootLayoutRequest and create the rootlayout
+        *   either non-async or asycn depending on the state of the GPU resource thread.
+        */
         void GPUResourceThread::ProcessRootLayoutRequest(RootLayoutRequest * request)
         {
             Resource::RootLayoutHandle handle = Resource::RootLayout::GetHandle(request->file, request->file);
@@ -173,6 +287,14 @@ namespace Hatchit
             }
         }
 
+        /**
+        *   \fn GPUResourceThread::ProcessPipelineRequest()
+        *   \brief Function processes a pipeline request
+        *   \param request Pointer to PipelineRequest
+        *
+        *   This function will process a PipelineRequst and create the pipeline
+        *   either non-async or asycn depending on the state of the GPU resource thread.
+        */
         void GPUResourceThread::ProcessPipelineRequest(PipelineRequest * request)
         {
             Resource::PipelineHandle handle = Resource::Pipeline::GetHandle(request->file, request->file);
@@ -188,6 +310,14 @@ namespace Hatchit
             }
         }
 
+        /**
+        *   \fn GPUResourceThread::ProcessShaderRequest()
+        *   \brief Function processes a shader request
+        *   \param request Pointer to ShaderRequest
+        *
+        *   This function will process a ShaderRequest and create the shader
+        *   either non-async or async depending on the state of the GPU resource thread.
+        */
         void GPUResourceThread::ProcessShaderRequest(ShaderRequest * request)
         {
             Resource::ShaderHandle handle = Resource::Shader::GetHandle(request->file, request->file);
@@ -203,6 +333,14 @@ namespace Hatchit
             }
         }
 
+        /**
+        *   \fn GPUResourceThread::ProcessRenderPassRequest()
+        *   \brief Function processes a renderpass request
+        *   \param request Pointer to RenderPassRequest
+        *
+        *   This function will process a RenderPassRequest and create the renderpass
+        *   either non-async or async depending on the state of the GPU resource thread.
+        */
         void GPUResourceThread::ProcessRenderPassRequest(RenderPassRequest* request)
         {
             Resource::RenderPassHandle handle = Resource::RenderPass::GetHandle(request->file, request->file);
@@ -217,6 +355,15 @@ namespace Hatchit
                 VCreateRenderPassBase(handle, request->data);
             }
         }
+        
+        /**
+        *   \fn GPUResourceThread::ProcessRenderTargetRequest()
+        *   \brief Function processes a rendertarget request
+        *   \param request Pointer to RenderTargetRequest
+        *
+        *   This function will process a RenderTargetRequest and create the rendertarget
+        *   either non-async or async depending on the state of the GPU resource thread.
+        */
         void GPUResourceThread::ProcessRenderTargetRequest(RenderTargetRequest* request)
         {
             Resource::RenderTargetHandle handle = Resource::RenderTarget::GetHandle(request->file, request->file);
@@ -231,6 +378,15 @@ namespace Hatchit
                 VCreateRenderTargetBase(handle, request->data);
             }
         }
+
+        /**
+        *   \fn GPUResourceThread::ProcessMeshRequest()
+        *   \brief Function processes a mesh request
+        *   \param request Pointer to MeshRequest
+        *
+        *   This function will process a MeshRequest and create the mesh
+        *   either non-async or async depending on the state of the GPU resource thread.
+        */
         void GPUResourceThread::ProcessMeshRequest(MeshRequest* request)
         {
             Resource::ModelHandle handle = Resource::Model::GetHandle(request->file, request->file);
