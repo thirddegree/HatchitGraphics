8e1bca94686cc94659295a9dcf4ae43fa0a08f97
Jhonny Knaak de Vargas <lomaisdoro@gmail.com>
2016-11-05 11:19:39 -0200

    Just added the license and author info in the file.

--- include/ht_mesh.h
+++ include/ht_mesh.h
@@ -1,3 +1,24 @@
+/**
+ **    Hatchit Engine
+ **    Copyright(c) 2015-2016 Third-Degree
+ **
+ **    GNU Lesser General Public License
+ **    This file may be used under the terms of the GNU Lesser
+ **    General Public License version 3 as published by the Free
+ **    Software Foundation and appearing in the file LICENSE.LGPLv3 included
+ **    in the packaging of this file. Please review the following information
+ **    to ensure the GNU Lesser General Public License requirements
+ **    will be met: https://www.gnu.org/licenses/lgpl.html
+ **
+ **/
+
+/**
+ * \file ht_mesh.h
+ * \brief Some new structures for mesh
+ * \author Jhonny Knaak de Vargas (lomaisdoro@gmail.com)
+ *
+ */
+
 #pragma once
 
 #include <glm/glm.hpp>
